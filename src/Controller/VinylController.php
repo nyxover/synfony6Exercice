116048e54893f22f679178c8a2d6be9da6092c85
BUIRETTE <pierre.buirette@cgi.com>
2023-03-06 15:59:15 +0100

    Add twiggy page

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Psychosocial', 'artist' =>  'slipknot'],
+            ['song' => '9eme symphonie', 'artist' =>  'Mozart'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
